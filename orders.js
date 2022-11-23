49d207bf8a796273c8cf4350744748833bbc0f21
Rasheed <105211312+RogueRasheed@users.noreply.github.com>
2022-11-23 10:18:09 +0100

    uploads

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Larvender DRONE',
+        productNumber: '28392',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Bluetooth Speakers',
+        productNumber: '10002',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Samsung A20s',
+        productNumber: '40030',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Iphone 13 Pro',
+        productNumber: '30921',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Samsung S30',
+        productNumber: '93848',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
